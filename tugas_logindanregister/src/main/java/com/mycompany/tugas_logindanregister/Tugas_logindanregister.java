2434303aab2b8b8dca2f8899e54f37f1d8a7b3d1
abimanyupewe <abimanyupw369@gmail.com>
2024-12-27 21:30:06 +0700

    update

---
+++ tugas_logindanregister/src/main/java/com/mycompany/tugas_logindanregister/Tugas_logindanregister.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas_logindanregister;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Tugas_logindanregister {
+
+    public static void main(String[] args) {
+        
+        Login Lg = new Login();
+        Lg.setVisible(true);
+        
+        Lg.pack();
+        Lg.setLocationRelativeTo(null);
+        Lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+    }
+}
